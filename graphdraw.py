b3f423a35f4f129718124caf8914b0e22ad1d506
Kester Jarvis <kester.jarvis@gmail.com>
2016-01-13 15:19:58 +0000

    Added sizes to node labels

--- graphdraw.py
+++ graphdraw.py
@@ -1,6 +1,4 @@
 import networkx as nx
-#import graphviz
-#import pydotplus as pydot
 import csv
 
 
@@ -15,7 +13,7 @@ def normalise_dict(dict1):
 
 
 def set_node_sizes(dot1, dict1):
-	#Needs to take a dot file (containing the graph) and a dictionary of values with same keys
+	#Needs to take a dot object (containing the graph) and a dictionary of values with same keys
 	#as the node names in dot file, and set relative node sizes based on the values associated
 	#with them in the dict. Modifies the dict sent in arguments rather than returning a new one.
 	sizes = normalise_dict(dict1)
@@ -24,30 +22,40 @@ def set_node_sizes(dot1, dict1):
 		node.set_width(sizes[node.get_name()])
 
 
-def produce_graph(dotgraph, sizedict, filename):
-	#Takes a Dot class produced by to_pydot, and a series of numbers with the same indices as the node
-	#names in the Dot class; writes a jpg file with nodes of size determined by the series.
+def set_node_names(dot1, dict1):
+	#Takes the same arguments as set_node_sizes, and simply adds the (original, not normalised) sizes
+	#to the name of each node.
+	for node in dot1.get_nodes():
+		name = node.get_name()
+		node.set_label(name[:-1]+'\n\nCount: '+dict1[name]+'"')
+		print(node.get_label())
+
+def produce_graph(nxgraph, sizedict, filename):
+	#Takes a Networkx graph, and a series of numbers with the same indices as the node
+	#names; writes a jpg file with nodes of size determined by the series.
+	dotgraph = nx.to_pydot(nxgraph)
 	set_node_sizes(dotgraph, sizedict)
+	set_node_names(dotgraph, sizedict)
+	dotgraph.set_nodesep(1)
 	dotgraph.write_jpg(filename)
+	"""
 	x = [i.get_height() for i in dotgraph.get_nodes()]
 	y = [i.get_width() for i in dotgraph.get_nodes()]
 	z = [i.get_name() for i in dotgraph.get_nodes()]
 	print("Names: ", z)
 	print("Heights: ", x)
 	print("Widths: ", y)
-
-
+	"""
 
 def main():
 	cassettes = nx.read_graphml("cassettes.graphml")
-	pydotcassettes = nx.to_pydot(cassettes)
+	#pydotcassettes = nx.to_pydot(cassettes)
 	with open('all-cassettes-table.csv', 'r') as in_hndl:
 		csv1 = [r for r in csv.DictReader(in_hndl)]
 	sizedict = {}
 	for cassette in csv1:
 		sizedict['"'+cassette['pattern']+'"'] = cassette['control']
-	pydotcassettes.set_nodesep(1)
-	produce_graph(pydotcassettes, sizedict, 'examplegraphcont.jpg')
+	produce_graph(cassettes, sizedict, 'examplegraphcont2.jpg')
 
 
 main()
